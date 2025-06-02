d8ea0c9f0c9586655a2438cb6902d253a5e08f5e
Rory Conlin <roryconlin@gmail.com>
2025-06-02 14:40:16 -0400

    Add stuff to init so it can be imported

--- yancc/__init__.py
+++ yancc/__init__.py
@@ -1,3 +1,15 @@
 """yancc: Yet Another NeoClassical Code."""
 
-from . import collisions, linalg, misc, preconditioner, trajectories, velocity_grids
+from . import (
+    collisions,
+    field,
+    krylov,
+    linalg,
+    misc,
+    multigrid,
+    preconditioner,
+    smoothers,
+    species,
+    trajectories,
+    velocity_grids,
+)
