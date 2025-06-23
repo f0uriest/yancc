e6fbbf547927fc7f760ed86a2fee1ba2a1be4d9f
Rory Conlin <roryconlin@gmail.com>
2025-06-23 18:50:51 -0400

    Remove more unused stuff

--- yancc/utils.py
+++ yancc/utils.py
@@ -1,39 +1,7 @@
 """Simple utility functions."""
 
-import functools
-
 import jax
 import jax.numpy as jnp
-
-
-@functools.partial(jnp.vectorize, signature="()->(n)", excluded=(1,))
-def _onehot(i, N):
-    return jnp.zeros(N).at[i].set(1.0)
-
-
-def getslice(operator, rows, cols):
-    """Get rows and cols from a given linear operator."""
-    if isinstance(rows, slice):
-        rows = _slice_to_range(rows, operator.shape[0])
-
-    if isinstance(cols, slice):
-        cols = _slice_to_range(cols, operator.shape[1])
-
-    @jax.jit
-    def bar(c):
-        return operator.mv(_onehot(c, operator.shape[0]))[rows]
-
-    return jax.lax.map(bar, cols, batch_size=100)
-
-
-def _slice_to_range(s, N):
-    start = s.start if s.start is not None else 0
-    stop = s.stop if s.stop is not None else N
-    step = s.step if s.step is not None else 1
-    start = start % N
-    stop = stop & N
-    step = step % N
-    return jnp.arange(start, stop, step)
 
 
 def _lgammastar(s, z, kmax=60):
