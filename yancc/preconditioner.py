3656a7836d3a4d573376a8842892808752d10e5b
Rory Conlin <roryconlin@gmail.com>
2025-09-03 14:59:16 -0400

    Add DKEM preconditioner

--- yancc/preconditioner.py
+++ yancc/preconditioner.py
@@ -1,6 +1,7 @@
 """Stuff for preconditioners."""
 
 import equinox as eqx
+import jax
 import jax.numpy as jnp
 import lineax as lx
 from jaxtyping import Array, ArrayLike, Float
@@ -15,7 +16,7 @@ from .multigrid import (
     get_mdke_jacobi_smoothers,
     get_mdke_operators,
 )
-from .species import LocalMaxwellian
+from .species import LocalMaxwellian, collisionality
 from .velocity_grids import AbstractSpeedGrid, UniformPitchAngleGrid
 
 
@@ -234,5 +235,123 @@ class DKEPreconditioner(MultigridOperator):
 @lx.is_symmetric.register(DKEPreconditioner)
 @lx.is_diagonal.register(DKEPreconditioner)
 @lx.is_tridiagonal.register(DKEPreconditioner)
+def _(operator):
+    return False
+
+
+class DKEMPreconditioner(lx.AbstractLinearOperator):
+    """Preconditioner for the DKE using block diagonal MDKE preconditioners."""
+
+    field: Field
+    pitchgrid: UniformPitchAngleGrid
+    speedgrid: AbstractSpeedGrid
+    species: list[LocalMaxwellian]
+    E_psi: Float[Array, ""]
+    M: MultigridOperator
+    vs: jax.Array
+
+    def __init__(
+        self,
+        field: Field,
+        pitchgrid: UniformPitchAngleGrid,
+        speedgrid: AbstractSpeedGrid,
+        species: list[LocalMaxwellian],
+        E_psi: Float[ArrayLike, ""],
+        **options
+    ):
+
+        self.field = field
+        self.pitchgrid = pitchgrid
+        self.speedgrid = speedgrid
+        self.species = species
+        self.E_psi = jnp.asarray(E_psi)
+
+        Ers = []
+        nus = []
+        vs = []
+        for spec in species:
+            temp_nu = []
+            temp_Er = []
+            temp_vs = []
+            for x in speedgrid.x:
+                v = x * spec.v_thermal
+                nu = collisionality(spec, v, *species)
+                Erhat = E_psi / v
+                nuhat = nu / v
+                temp_Er.append(Erhat)
+                temp_nu.append(nuhat)
+                temp_vs.append(v)
+
+            Ers.append(temp_Er)
+            nus.append(temp_nu)
+            vs.append(temp_vs)
+
+        Ers = jnp.array(Ers)
+        nus = jnp.array(nus)
+        self.vs = jnp.array(vs)
+
+        def get_mdke_precond(nu, E_psi):
+            return MDKEPreconditioner(
+                field=field, pitchgrid=pitchgrid, E_psi=E_psi, nu=nu, **options
+            )
+
+        self.M = jax.vmap(jax.vmap(get_mdke_precond))(nus, Ers)
+
+    @eqx.filter_jit
+    def mv(self, vector):
+        """Matrix-vector product."""
+        vector = vector.reshape((len(self.species), self.speedgrid.nx, -1))
+
+        def _mv(M, v):
+            return M.mv(v)
+
+        out = jax.vmap(jax.vmap(_mv))(self.M, vector)
+        out = out / self.vs[:, :, None]
+        return out.flatten()
+
+    def as_matrix(self):
+        """Materialize the operator as a dense matrix."""
+        x = jnp.zeros(self.in_size())
+        return jax.jacfwd(self.mv)(x)
+
+    def in_structure(self):
+        """Pytree structure of expected input."""
+        return jax.ShapeDtypeStruct(
+            (
+                self.field.ntheta
+                * self.field.nzeta
+                * self.pitchgrid.nxi
+                * self.speedgrid.nx
+                * len(self.species),
+            ),
+            dtype=self.field.Bmag.dtype,
+        )
+
+    def out_structure(self):
+        """Pytree structure of expected output."""
+        return jax.ShapeDtypeStruct(
+            (
+                self.field.ntheta
+                * self.field.nzeta
+                * self.pitchgrid.nxi
+                * self.speedgrid.nx
+                * len(self.species),
+            ),
+            dtype=self.field.Bmag.dtype,
+        )
+
+    def transpose(self):
+        """Transpose of the operator."""
+        x = jnp.zeros(self.in_size())
+
+        def fun(y):
+            return jax.linear_transpose(self.mv, x)(y)[0]
+
+        return lx.FunctionLinearOperator(fun, x)
+
+
+@lx.is_symmetric.register(DKEMPreconditioner)
+@lx.is_diagonal.register(DKEMPreconditioner)
+@lx.is_tridiagonal.register(DKEMPreconditioner)
 def _(operator):
     return False
